16954d524d4cfa1b1dc4aa3f6924f1265e849b61
starryeye <see2818@naver.com>
2023-10-20 22:58:07 +0900

    [starryeye, 2023.10.20] - webhandler study init
    내용
    - webhandler study init

---
+++ webhandler/src/main/java/dev/practice/webhandler/WebhandlerApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.webhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhandlerApplication.class, args);
+	}
+
+}
---
+++ webhandler/src/test/java/dev/practice/webhandler/WebhandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.webhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
