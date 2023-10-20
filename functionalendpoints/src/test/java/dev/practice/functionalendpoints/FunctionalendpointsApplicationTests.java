e7914113e3a91752860e2dfc9dfde3a91645918f
starryeye <see2818@naver.com>
2023-10-21 04:07:40 +0900

    [starryeye, 2023.10.21] - functional endpoints study init
    내용
    - functional endpoints study init

---
+++ functionalendpoints/src/test/java/dev/practice/functionalendpoints/FunctionalendpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.functionalendpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalendpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
