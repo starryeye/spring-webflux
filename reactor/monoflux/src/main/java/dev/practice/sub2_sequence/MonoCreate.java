cc5da22593d6405e2f75c366909a2ffdd6cea7ec
starryeye <see2818@naver.com>
2023-10-23 22:36:08 +0900

    [starryeye, 2023.10.23] - Reactor Mono, Flux study generate, create
    내용
    - generate, create

--- reactor/monoflux/src/main/java/dev/practice/sub2_sequence/MonoCreate.java
+++ reactor/monoflux/src/main/java/dev/practice/sub2_sequence/MonoCreate.java
@@ -10,6 +10,7 @@ public class MonoCreate {
      *
      * TODO
      * Mono 가 제공하는 create 메서드에 대해 알아본다.
+     * MonoSink 에는 success 가 존재.. FluxSink 가 제공하는 메서드와 차이가 있는듯..
      */
 
     public static void main(String[] args) {
