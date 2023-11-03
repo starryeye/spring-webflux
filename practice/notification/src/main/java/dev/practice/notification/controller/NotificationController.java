f2f1f37e58ad7bae7d8078b724027c5226e2e21b
starryeye <see2818@naver.com>
2023-11-03 20:37:40 +0900

    [starryeye, 2023.11.03] - 간단한 server sent event 서버 v2
    내용
    - Sinks.Many<T>

--- practice/notification/src/main/java/dev/practice/notification/controller/NotificationController.java
+++ practice/notification/src/main/java/dev/practice/notification/controller/NotificationController.java
@@ -1,16 +1,26 @@
 package dev.practice.notification.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/notifications")
+@RequestMapping("/api/notifications/v1")
 public class NotificationController {
+
+    /**
+     * Server Sent Event
+     * - Http streaming 기법이다.
+     * - Chunked Transfer-Encoding 기반이다.
+     * - chunk 단위(여러 줄로 구성된 문자열이며 new line 으로 서로를 구분)로 event 를 구분
+     * 즉, chunk 하나가 event 하나
+     * - 문자열은 일반적으로 field: value 로 구성
+     */
+
 
     /**
      * Server Sent Event 를 구현하기 위한 조건
@@ -18,8 +28,8 @@ public class NotificationController {
      * 1. Content-Type: text/event-stream
      * -> server sent event 의 조건이다.
      *
-     * 2. Flux<String> 을 리턴타입
-     * -> Chunk 단위 내에서 id 는 없고.. event 타입은 message(기본 값) 로 전달하도록 함
+     * 2. Flux<String> 을 리턴타입으로 사용 (Flux 의 타입 파라미터가 ServerSentEvent 가 아님)
+     * -> Chunk 단위 내에서 id 는 없고.. "event" 타입은 "message"(기본 값) 로 전달하도록 함
      * ServerSentEvent 객체의 data 에 String 을 집어 넣고 쭉 흘려보낸다.
      *
      * 3. @ResponseBody
---
+++ practice/notification/src/main/java/dev/practice/notification/controller/NotificationControllerV2.java
@@ -0,0 +1,51 @@
+package dev.practice.notification.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/notifications/v2")
+public class NotificationControllerV2 {
+
+    /**
+     * Sinks.many() : Flux..
+     * unicast() : 하나의 subscriber 가 존재한다.
+     * onBackpressureBuffer() : ??..
+     *
+     * 흠..
+     * 나중에 값을 집어넣을 수 있는
+     * 파이프 라인만 일단 만들어두는 느낌..
+     */
+    private Sinks.Many<String> sink = Sinks.many().unicast().onBackpressureBuffer();
+
+    // 이벤트 생성 API
+    @PostMapping
+    public Mono<String> addNotification(@RequestBody Event event) {
+
+        String notificationMessage = event.getType() + ": " + event.getMessage();
+
+        log.info("addNotification, message: {}, tx: {}", notificationMessage, Thread.currentThread().getName());
+
+        sink.tryEmitNext(notificationMessage); // 값을 집어 넣는다.
+
+        return Mono.just("ok");
+    }
+
+    /**
+     * Flux 의 타입 파라미터가 String 으로.. ServerSentEvent 가 아니기 때문에
+     * String 이 data 로 그대로 내려간다.
+     * 따라서, id 는 없으며.. "event" 타입은 "message"(기본 값) 로 내려간다.
+     */
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getNotifications() {
+
+        // 클라이언트가.. 구독을 하고 있는 격이 된다...
+
+        return sink.asFlux(); // sink 를 flux 로 넘겨준다.
+    }
+}
