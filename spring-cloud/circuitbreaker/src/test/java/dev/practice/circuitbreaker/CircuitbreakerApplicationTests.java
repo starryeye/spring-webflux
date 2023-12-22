c94c13ded6eb53418ad14bb97248cafc2b6b61fa
starryeye <see2818@naver.com>
2023-12-22 21:08:29 +0900

    [starryeye, 2023.12.22] - spring cloud circuit breaker init
    내용
    - spring cloud 에서 제공하는 circuit breaker
    - implement : resilience4j (for reactive)

---
+++ spring-cloud/circuitbreaker/src/test/java/dev/practice/circuitbreaker/CircuitbreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
