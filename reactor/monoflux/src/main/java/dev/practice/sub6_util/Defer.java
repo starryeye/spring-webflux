3f421f9afed306493d69a5fa7b04041108d38b9f
starryeye <see2818@naver.com>
2024-03-14 23:19:08 +0900

    [starryeye, 2024.03.14] - Reactor Mono, Flux study 기타 연산자
    내용
    - 기타 연산자

--- reactor/monoflux/src/main/java/dev/practice/sub6_util/Defer.java
+++ reactor/monoflux/src/main/java/dev/practice/sub6_util/Defer.java
@@ -9,13 +9,15 @@ public class Defer {
     /**
      * [9]
      *
-     * Defer 연산자에 대해 알아본다.
+     * defer 연산자에 대해 알아본다.
      * public static <T> Mono<T> defer(Supplier<? extends Mono<? extends T>> supplier)
      *
      * - publisher 를 생성하는 Supplier 를 인자로 받는다.
      * - 생성된 publisher 의 이벤트를 downstream 으로 전달한다.
      * - flatMap 과 차이점은.. 둘다 publisher 를 반환하는 것은 동일하지만..
-     * flatMap 은 Functional 이지만, Defer 는 Supplier 이다.
+     * flatMap 은 Functional 이지만, defer 는 Supplier 이다.
+     * -> flatMap 은 파이프라인 중간에 위치하며 onNext 에 의해 반응
+     * -> defer 는 파이프라인 중간에 위치하지는 않으며, publisher 를 생성해내는 역할이다.
      */
 
     public static void main(String[] args) {
