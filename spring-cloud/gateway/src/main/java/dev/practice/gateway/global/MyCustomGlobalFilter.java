706ea579cc213b5a62a68f2e689db4d582f2bbfd
starryeye <see2818@naver.com>
2024-04-18 00:25:23 +0900

    [starryeye, 2024.04.18] - reactive gateway, GlobalFilter
    내용
    - reactive gateway, GlobalFilter

--- spring-cloud/gateway/src/main/java/dev/practice/gateway/global/MyCustomGlobalFilter.java
+++ spring-cloud/gateway/src/main/java/dev/practice/gateway/global/MyCustomGlobalFilter.java
@@ -4,9 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
 
 @Slf4j
 @Component
@@ -26,9 +32,13 @@ public class MyCustomGlobalFilter implements GlobalFilter, Ordered {
      */
 
     /**
-     * change the URI dynamically
+     * Change the URI dynamically
+     * - spring cloud gateway 에서는 application.yml 이나 설정으로 편하게..
+     * URI 를 조건에따라 런타임에 switching 하는 기능이 없어서 아래와 같이 GlobalFilter 를 이용해야하는 것 같다..
      *
      * https://github.com/spring-cloud/spring-cloud-gateway/issues/3119
+     * -> GATEWAY_REQUEST_URL_ATTR 를 이용하여 URI 를 변경시키고, Order 는 10001 로 해야 정확히 동작한다.
+     * -> 즉, RouteToRequestUrlFilter(order : 10000)가 동작한 이후에 동작해야하는 것임
      *
      * 참고
      * https://yangbongsoo.tistory.com/14
@@ -36,11 +46,40 @@ public class MyCustomGlobalFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return null;
+
+        // http body 에 "A" 가 포함되면 "http://localhost:8090", 그외 "http://localhost:8091" 로 요청하도록 한다.
+
+        return DataBufferUtils.join(exchange.getRequest().getBody())
+                .flatMap(
+                        dataBuffer -> {
+                            byte[] bytes = new byte[dataBuffer.readableByteCount()];
+                            dataBuffer.read(bytes);
+                            DataBufferUtils.release(dataBuffer);
+
+                            String bodyStr = new String(bytes);
+
+                            ServerHttpRequest mutatedRequest;
+
+                            if(bodyStr.contains("A")) {
+                                mutatedRequest = exchange.getRequest()
+                                        .mutate()
+                                        .uri(URI.create("http://localhost:8090"))
+                                        .build();
+                            }else {
+                                mutatedRequest = exchange.getRequest()
+                                        .mutate()
+                                        .uri(URI.create("http://localhost:8091"))
+                                        .build();
+                            }
+                            exchange.getAttributes()
+                                    .put(GATEWAY_REQUEST_URL_ATTR, mutatedRequest.getURI());
+                            return chain.filter(exchange.mutate().request(mutatedRequest).build());
+                        }
+                ).switchIfEmpty(chain.filter(exchange));
     }
 
     @Override
     public int getOrder() {
-        return 0;
+        return 10001;
     }
 }
