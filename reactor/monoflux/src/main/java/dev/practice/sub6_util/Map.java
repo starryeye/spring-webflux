4ea310f3124ccfc9a7a26418891c9f45129a83ff
starryeye <see2818@naver.com>
2024-04-18 23:58:33 +0900

    [starryeye, 2024.04.18] - 주석 추가
    내용
    - 주석 추가

--- reactor/monoflux/src/main/java/dev/practice/sub6_util/Map.java
+++ reactor/monoflux/src/main/java/dev/practice/sub6_util/Map.java
@@ -14,6 +14,8 @@ public class Map {
      * map
      * public final <V> Flux<V> map(Function<? super T, ? extends V> mapper)
      * - onNext 이벤트를 받아서 값을 변경하고 downstream 으로 전달한다.
+     * - todo, map vs flatMap.. 동기 비동기 측면에서 차이가 있다고 한다.
+     *      비동기 IO 를 수행하는 메서드를 map 으로 감싸고 flatMap 으로 감싸고 두 경우를 각각 테스트 해보면서, 어떤 스레드로 동작되는지 확인해볼것
      *
      * mapNotNull
      * public final <V> Flux<V> mapNotNull(Function <? super T, ? extends V> mapper)
