ae213d36a3521fcfd083b0f068a07e13a97ec12d
starryeye <see2818@naver.com>
2024-07-04 17:20:09 +0900

    [starryeye, 2024.07.04] - Spring Data R2dbc initialization
    내용
    - todo list

--- data/r2dbc/initialization/src/main/java/dev/starryeye/initialization/InitializationApplication.java
+++ data/r2dbc/initialization/src/main/java/dev/starryeye/initialization/InitializationApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InitializationApplication {
 
+	/**
+	 * todo, r2dbc 를 이용한 DB 연동과 기본 테스트
+	 * 	- application.yml 을 이용
+	 * 	- Configuration 을 이용
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(InitializationApplication.class, args);
 	}
