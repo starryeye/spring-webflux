831cfa1d84027e0e3d1634e52a628c31f2512de2
starryeye <see2818@naver.com>
2024-07-04 17:18:44 +0900

    [starryeye, 2024.07.04] - Spring Data R2dbc initialization
    내용
    - Spring Data R2dbc initialization

---
+++ data/r2dbc/initialization/src/main/java/dev/starryeye/initialization/InitializationApplication.java
@@ -0,0 +1,13 @@
+package dev.starryeye.initialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializationApplication.class, args);
+	}
+
+}
