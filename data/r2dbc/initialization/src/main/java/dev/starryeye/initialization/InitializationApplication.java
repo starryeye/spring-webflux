3dbefeaa21324602e321b233d9136b551ab0a49b
starryeye <see2818@naver.com>
2024-07-11 22:14:52 +0900

    [starryeye, 2024.07.11] - R2dbc save
    내용
    - R2dbc save

--- data/r2dbc/initialization/src/main/java/dev/starryeye/initialization/InitializationApplication.java
+++ data/r2dbc/initialization/src/main/java/dev/starryeye/initialization/InitializationApplication.java
@@ -9,7 +9,9 @@ public class InitializationApplication {
 	/**
 	 * todo, r2dbc 를 이용한 DB 연동과 기본 테스트
 	 * 	- application.yml 을 이용
+	 * 		save, update, delete, find.. test code
 	 * 	- Configuration 을 이용
+	 * 		이건 ConnectionFactory 다루면서 해보자..
 	 */
 
 	public static void main(String[] args) {
