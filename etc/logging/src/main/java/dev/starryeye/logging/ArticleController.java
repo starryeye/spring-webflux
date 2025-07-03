1d2647d4ef1e81a54aef412824762f6bef5454b7
starryeye <see2818@naver.com>
2025-07-03 23:47:54 +0900

    [starryeye, 2025.07.03] - logging, practice
    내용
    -  logging, practice

--- etc/logging/src/main/java/dev/starryeye/logging/ArticleController.java
+++ etc/logging/src/main/java/dev/starryeye/logging/ArticleController.java
@@ -114,6 +114,14 @@ public class ArticleController {
                     log.info("response article..");
                     return ResponseEntity.ok(articleResponse);
                 });
+
+        /**
+         * 동작 과정
+         * (1) Mono 생성과 동시에 Subscription 이 생김
+         * (2) 10 s 타이머 동안 client connection 연결이 끊기면
+         *      Netty -> Reactor 로 cancel 시그널 전달
+         *      타이머 dispose -> map 이하 미실행
+         */
     }
 
     @GetMapping("/sleep-and-disconnect-2")
@@ -123,8 +131,21 @@ public class ArticleController {
         // 체인이 생성 및 반환(return Mono.just)되기 전 connection cancel 시, 체인이 없으므로 cancel 무시됨.
         // Netty 에서는 채널을 closed로 표시. 그래도 생성된 체인에 대해서는 subscribe 를 수행한다.
 
+        // Netty 는 controller 로 부터 publisher 를 반환 받고 subscribe 해서 subscription 으로 동작을 수행시키는데..
+        // Publisher 를 반환 받지도 않은 상태이므로 subscription 도 없는데 cancel 시그널을 발생 시킬수 조차 없는 것이다.
+
         ArticleResponse articleResponse = new ArticleResponse("title", "content");
 
         return Mono.just(ResponseEntity.ok(articleResponse));
+
+        /**
+         * 동작 과정
+         * (1) 이 10 s 동안은 아직 Publisher, Subscription 모두 없음
+         *      연결이 끊겨도 cancel 시그널을 보낼 대상 자체가 없다
+         * (2) 10 s 후 Mono.just 반환
+         *      WebFlux 가 어쨌든 subscribe 시도
+         *      Mono.just 는 onNext -> onComplete 수행
+         *      Netty 채널은 이미 CLOSED 상태라 write 실패
+         */
     }
 }
