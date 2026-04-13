cf52fcb97243b9998b4913b79f2ae236bda73715
starryeye <starrystarryeye@gmail.com>
2026-04-13 22:15:03 +0900

    [starryeye, 2026.04.13] - kotlin coroutines
    내용
    - kotlin coroutines

---
+++ with-kotlin/hello-coroutine/src/main/kotlin/dev/starryeye/hello_coroutine/sub1_library/Description.kt
@@ -0,0 +1,17 @@
+package dev.starryeye.hello_coroutine.sub1_library
+
+/**
+ * Kotlin 라이브러리
+ *
+ * Kotlin Coroutine
+ *      kotlinx.coroutines 패키지로 지원한다.
+ *      동시성과 관련하여 강력한 성능과 개발 편의를 제공한다.
+ *
+ * Kotlin Serialization
+ *      kotlinx.serialization 패키지로 지원한다.
+ *      객체의 serialization, deserialization 을 지원한다. (Jackson 처럼..)
+ *
+ * Kotlin Lincheck
+ *      kotlinx.lincheck 패키지로 지원한다.
+ *      동시성 버그를 탐지하고 디버그할 수 있게 테스트 환경을 제공한다.
+ */
---
+++ with-kotlin/hello-coroutine/src/main/kotlin/dev/starryeye/hello_coroutine/sub2_kotlin_coroutine/Description.kt
@@ -0,0 +1,16 @@
+package dev.starryeye.hello_coroutine.sub2_kotlin_coroutine
+
+/**
+ * Kotlin Coroutines
+ *
+ * 동시성을 지원하는 라이브러리이다.
+ * 비동기 non-blocking 으로 동작하는 코드를 "동기 코드 처럼" 보이게한다.
+ *
+ * CoroutineContext 를 통해..
+ *      dispatcher, error handling, threadLocal 등을 지원한다.
+ *
+ * CoroutineScope 를 통해..
+ *      structured concurrency 와 cancellation 을 제공한다.
+ *
+ * flow, channel 등의 심화 기능을 제공하낟.
+ */
