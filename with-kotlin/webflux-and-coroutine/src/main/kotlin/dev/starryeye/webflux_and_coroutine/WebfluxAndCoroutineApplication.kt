82a35f42f7c32dafcbcd4340cb705b42ce271051
starryeye <starrystarryeye@gmail.com>
2026-04-28 20:00:09 +0900

    [starryeye, 2026.04.28] - kotlin coroutines
    내용
    - kotlin coroutines

---
+++ with-kotlin/webflux-and-coroutine/src/main/kotlin/dev/starryeye/webflux_and_coroutine/WebfluxAndCoroutineApplication.kt
@@ -0,0 +1,11 @@
+package dev.starryeye.webflux_and_coroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxAndCoroutineApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebfluxAndCoroutineApplication>(*args)
+}
---
+++ with-kotlin/webflux-and-coroutine/src/test/kotlin/dev/starryeye/webflux_and_coroutine/WebfluxAndCoroutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.starryeye.webflux_and_coroutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebfluxAndCoroutineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
